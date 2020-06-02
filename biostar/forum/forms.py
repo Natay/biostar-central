d45d8858610aa1995607775b0c64857577e22e13
Natay Aberra <natay.aberra@gmail.com>
2020-06-02 11:03:14 -0400

    update bar

--- biostar/forum/forms.py
+++ biostar/forum/forms.py
@@ -50,24 +50,31 @@ def valid_tag(text):
         raise ValidationError('You have too many tags (5 allowed)')
 
 
+def validate_tags_packages(text):
+    """
+    Ensure tags contain at least one package name.
+    """
+
+    # Get the tags file to check against.
+
+
+    return
+
+
 class PostLongForm(forms.Form):
 
     choices = [opt for opt in Post.TYPE_CHOICES if opt[0] in Post.TOP_LEVEL]
 
-    # mapper = {
-    #           Post.QUESTION: "Ask a question", Post.TUTORIAL: "Share a Tutorial",
-    #           Post.JOB: "Post a Job Opening", Post.FORUM: "Start a Discussion",
-    #           Post.TOOL: "Share a Tool", Post.NEWS: "Announce News"
-    #           }
+    mapper = {
+              Post.QUESTION: "Ask a question", Post.TUTORIAL: "Share a Tutorial",
+              Post.JOB: "Post a Job Opening", Post.FORUM: "Start a Discussion",
+              Post.TOOL: "Share a Tool", Post.NEWS: "Announce News"
+              }
     if settings.ALLOWED_POST_TYPES:
         choices = [opt for opt in choices if opt[1] in settings.ALLOWED_POST_TYPES]
 
-    # if settings.REMAP_TYPE_DISPLAY:
-    #     type_choices = []
-    #     for c in choices:
-    #         type_choices.append((c[0], mapper.get(c[0], c[1])))
-    # else:
-    #     type_choices = choices
+    for idx ,opt in enumerate(choices):
+        choices[idx] = (opt[0], mapper.get(opt[0], opt[1]))
 
     post_type = forms.IntegerField(label="Post Type",
                                    widget=forms.Select(choices=choices, attrs={'class': "ui dropdown"}),
